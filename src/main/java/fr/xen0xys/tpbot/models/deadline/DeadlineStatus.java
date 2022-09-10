98004cee06d68e1d6b820d43e815b7f9d32e14ae
Xen0Xys <red57101@gmail.com>
2022-09-10 21:35:56 +0200

    :rocket: Add display field for DeadlineStatus enum

--- src/main/java/fr/xen0xys/tpbot/models/deadline/DeadlineStatus.java
+++ src/main/java/fr/xen0xys/tpbot/models/deadline/DeadlineStatus.java
@@ -2,21 +2,26 @@ package fr.xen0xys.tpbot.models.deadline;
 
 public enum DeadlineStatus {
 
-    MONTH(30*24*60*60L),
-    WEEK(7*24*60*60L),
-    THREE_DAYS(3*24*60*60L),
-    DAY(24*60*60L),
-    HOUR(60*60L),
-    TEN_MINUTES(10*60L),
-    ENDED(0L);
+    MONTH(30*24*60*60L, "1 Mois"),
+    WEEK(7*24*60*60L, "1 Semaine"),
+    THREE_DAYS(3*24*60*60L, "3 Jours"),
+    DAY(24*60*60L, "1 Jour"),
+    HOUR(60*60L, "1 Heure"),
+    TEN_MINUTES(10*60L, "10 Minutes"),
+    ENDED(0L, "Terminé");
 
     private final long msDuration;
+    private final String display;
 
-    DeadlineStatus(long msDuration){
+    DeadlineStatus(long msDuration, String display){
         this.msDuration = msDuration;
+        this.display = display;
     }
 
     public long getDuration() {
         return msDuration;
+    }
+    public String getDisplay() {
+        return display;
     }
 }
