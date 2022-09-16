4710d6378907353abbf6033a79fc3d5663b3b673
Xen0Xys <red57101@gmail.com>
2022-09-16 12:04:24 +0200

    :loud_sound: Add logging to a file in addition to the console

--- src/main/java/fr/xen0xys/tpbot/TPBot.java
+++ src/main/java/fr/xen0xys/tpbot/TPBot.java
@@ -9,21 +9,18 @@ import fr.xen0xys.tpbot.models.ModuleManager;
 import fr.xen0xys.tpbot.models.Utils;
 import fr.xen0xys.tpbot.models.deadline.DeadLine;
 import fr.xen0xys.tpbot.slashcommands.DeadLineSlashCommand;
-import fr.xen0xys.tpbot.slashcommands.EDTSlashCommand;
 import fr.xen0xys.xen0lib.database.Database;
 import fr.xen0xys.xen0lib.utils.Status;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
 
-import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.logging.*;
 
 public class TPBot {
     public static JDA bot;
@@ -37,11 +34,21 @@ public class TPBot {
     public static void main(String[] args){
         //Init
         InputStream stream = TPBot.class.getClassLoader().getResourceAsStream("logging.properties");
+        FileHandler logFileHandler;
         try {
             LogManager.getLogManager().readConfiguration(stream);
             logger = Logger.getLogger(TPBot.class.getName());
+            // Init file logger
+            LocalDateTime dateTime = LocalDateTime.now();
+            File logFile = new File(String.format("%s/logs/%s-%s-%s_%s-%s-%s.log", DATAFOLDER.getPath(), dateTime.getDayOfMonth(), dateTime.getMonthValue(), dateTime.getYear(), dateTime.getHour(), dateTime.getMinute(), dateTime.getSecond()));
+            logFile.getParentFile().mkdirs();
+            logFileHandler = new FileHandler(logFile.getPath());
+            SimpleFormatter formatter = new SimpleFormatter();
+            logFileHandler.setFormatter(formatter);
+            logger.addHandler(logFileHandler);
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(1);
         }
         config = new Config(DATAFOLDER, "config.yml");
 
@@ -63,7 +70,7 @@ public class TPBot {
         }
         if(database.connect() != Status.Success){
             logger.severe("Cannot connect to Database, stopping bot...");
-            System.exit(1);
+            System.exit(2);
         }
         deadLinesTable = new DeadLinesTable("tpbot_deadlines", database);
         database.openTableAndCreateINE(deadLinesTable, "id VARCHAR(6) NOT NULL PRIMARY KEY," +
@@ -79,7 +86,7 @@ public class TPBot {
 
             Guild guild = bot.getGuildById(getConfiguration().getGuildId());
             if(guild == null){
-                System.exit(1);
+                System.exit(3);
             }
             // Slash commands
             // guild.upsertCommand(new EDTSlashCommand().getCommandData()).queue();
@@ -88,7 +95,7 @@ public class TPBot {
             // Events
             bot.addEventListener(new SlashCommandListener());
             bot.addEventListener(new ModalInteractionListener());
-        } catch (InterruptedException | LoginException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
@@ -115,6 +122,7 @@ public class TPBot {
                     moduleManager.unloadModules();
                     database.disconnect();
                     bot.shutdownNow();
+                    closeLogger();
                 }
                 case "reload" -> {
                     config = new Config(DATAFOLDER, "config.yml");
@@ -123,6 +131,12 @@ public class TPBot {
                 }
             }
         } while (running);
+    }
+
+    private static void closeLogger(){
+        for(Handler handler: logger.getHandlers()){
+            handler.close();
+        }
     }
 
     public static Config getConfiguration(){
